fc903823d83ed11bfe1b47f8ca7f26af465129ab
AJEET69 <ajeetsinghverma69@gmail.com>
2025-09-09 21:07:41 +0530

    18 Dec

---
+++ README.md
@@ -0,0 +1,13 @@
+### ATB8x
+
+## Learning Selenium ATB8x
+
+Programs containing Selenium Learning Resource.
+
+**Topics**
+
+1. What is Selenium? 
+2. Features of Selenium? 
+3. Why Selenium, Advantages, DisAdvantage 
+4. How to install Selenium, use it. 
+5. Selenium Arch.(W3C, Json Wire protocol)
---
+++ src/test/java/com/thetestingacademy/ex01_18122024/Selenium01.java
@@ -0,0 +1,25 @@
+package com.thetestingacademy.ex01_18122024;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Selenium01 {
+
+
+    @Test
+    public void Test_Selenium(){
+
+
+        // In Selenium 3 for firefox browser
+     //        System.setProperty("webdriver.gecko.driver", "path/to/firefoxdriver");
+
+        // Selenium 4
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+
+
+
+    }
+
+
+}
