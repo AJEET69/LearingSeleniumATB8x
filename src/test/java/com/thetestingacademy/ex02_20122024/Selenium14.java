a3d372a53430c4e2db092a989dd7a5177bae1d1d
AJEET69 <ajeetsinghverma69@gmail.com>
2025-09-15 21:31:43 +0530

    20 Dec

---
+++ src/test/java/com/thetestingacademy/ex02_20122024/Selenium14.java
@@ -0,0 +1,24 @@
+package com.thetestingacademy.ex02_20122024;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Selenium14 {
+
+@Test
+    public  void TestMethod01(){
+
+
+    EdgeDriver driver = new EdgeDriver();
+//    driver.get("sdet.live");  fail
+  //  driver.get("https://sdet.live"); Pass
+  //   driver.get("http://sdet.live");  Pass
+
+//        driver.get("www.google.com"); fail
+
+
+
+
+}
+
+}
---
+++ src/test/java/com/thetestingacademy/ex02_20122024/Selenium15.java
@@ -0,0 +1,56 @@
+package com.thetestingacademy.ex02_20122024;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class Selenium15 {
+
+    @Test
+    public void testMethod1() throws InterruptedException, Exception {
+
+        // Open the https://katalon-demo-cura.herokuapp.com/
+        //  Verify the title and search a string in pageSource
+        // Add assertion that the cura health care Service is visible in the page source.
+
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--start-maximized");
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+
+
+       // Alternate way to maximize the window
+        // driver.manage().window().maximize();
+
+
+        // TestNG Assertion
+        Assert.assertEquals(driver.getTitle(), "CURA Healthcare Service");
+
+
+        // AssertJ Assertion
+        assertThat(driver.getTitle()).isEqualTo("CURA Healthcare Service").isNotBlank().isNotEmpty();
+        assertThat(driver.getCurrentUrl()).isEqualTo("https://katalon-demo-cura.herokuapp.com/");
+
+
+        if (driver.getPageSource().contains("CURA Healthcare Service")) {
+            Assert.assertTrue(true);
+        } else {
+            throw new Exception("Heading Not Found!!");
+        }
+
+
+        // "" - Empty , " " - blank
+
+
+        driver.quit();
+
+
+    }
+
+}
